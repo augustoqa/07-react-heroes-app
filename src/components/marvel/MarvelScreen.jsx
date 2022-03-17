03250e29cf36c2f732cc579c103104ac2cb71fef
César Acual <augustoacual@gmail.com>
2022-03-16 22:46:00 -0600

    Mostrar Listado de Heroes

--- src/components/marvel/MarvelScreen.jsx
+++ src/components/marvel/MarvelScreen.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
+import { HeroList } from '../heroes/HeroList'
 
 export const MarvelScreen = () => {
-  return <h1>Marvel Screen</h1>
+  return (
+    <>
+      <h1>Marvel Screen</h1>
+      <hr />
+      <HeroList publisher='Marvel Comics' />
+    </>
+  )
 }
