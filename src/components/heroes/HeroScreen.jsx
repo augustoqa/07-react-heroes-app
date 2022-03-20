ae2fbf20fc5364ffb4a8601a1b3c1ce0b7d411cf
César Acual <augustoacual@gmail.com>
2022-03-20 17:56:31 -0600

    Mostrar el detalle de un heroe

--- src/components/heroes/HeroScreen.jsx
+++ src/components/heroes/HeroScreen.jsx
@@ -1,5 +1,56 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min'
+import { getHeroById } from '../../selectors/getHeroById'
 
-export const HeroScreen = () => {
-  return <h1>HeroScreen</h1>
+export const HeroScreen = ({ history }) => {
+  const { heroeId } = useParams()
+  const hero = getHeroById(heroeId)
+
+  if (!hero) {
+    return <Redirect to='/' />
+  }
+
+  const handleReturn = () => {
+    history.length <= 2 ? history.push('/') : history.goBack()
+  }
+
+  const { superhero, publisher, alter_ego, first_appearance, characters } = hero
+
+  return (
+    <div className='row mt-5'>
+      <div className='col-4'>
+        <img
+          src={`../assets/heroes/${heroeId}.jpg`}
+          alt={superhero}
+          className='img-thumbnail'
+        />
+      </div>
+
+      <div className='col-8'>
+        <h3>{superhero}</h3>
+        <ul className='list-group list-group-flush'>
+          <li className='list-group-item'>
+            <b>Alter ego: </b>
+            {alter_ego}
+          </li>
+          <li className='list-group-item'>
+            <b>Publisher: </b>
+            {publisher}
+          </li>
+          <li className='list-group-item'>
+            <b>First appearance: </b>
+            {first_appearance}
+          </li>
+        </ul>
+
+        <h5>Characters</h5>
+        <p>{characters}</p>
+
+        <button onClick={handleReturn} className='btn btn-outline-info'>
+          Return
+        </button>
+      </div>
+    </div>
+  )
 }
