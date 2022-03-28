db29caf6e942427b6abe6612cd853b13b540ad56
César Acual <augustoacual@gmail.com>
2022-03-28 17:29:33 -0600

    Logout de usuario, usando hook useHistory de react-router-dom

--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,11 +1,23 @@
 import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Link, NavLink } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const Navbar = () => {
   const {
+    dispatch,
     user: { name },
   } = useContext(AuthContext)
+  const history = useHistory()
+
+  const handleLogout = () => {
+    history.replace('/login')
+
+    dispatch({
+      type: types.LOGOUT,
+    })
+  }
 
   return (
     <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
@@ -47,14 +59,9 @@ export const Navbar = () => {
       <div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
         <ul className='navbar-nav ml-auto'>
           <span className='nav-item nav-link text-info'>{name}</span>
-          <NavLink
-            activeClassName='active'
-            className='nav-item nav-link'
-            exact
-            to='/login'
-          >
+          <button onClick={handleLogout} className='nav-item nav-link btn'>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
