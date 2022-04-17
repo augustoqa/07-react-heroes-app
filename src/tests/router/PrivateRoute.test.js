e414204bc0537249ac5e490f9e2cabba05977a75
Cesar Acual <chechaacual@gmail.com>
2022-04-17 09:55:59 -0600

    Pruebas en el PrivateRoute

---
+++ src/tests/router/PrivateRoute.test.js
@@ -0,0 +1,25 @@
+import { mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom'
+import { PrivateRoute } from '../../router/PrivateRoute'
+
+describe('Pruebas en <PrivateRoute />', () => {
+  const props = {
+    location: {
+      pathname: '/marvel',
+    },
+  }
+
+  test('debe de mostrar el componente si esta autenticado y guardar localStorage', () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <PrivateRoute
+          isAuthenticated={true}
+          component={() => <span>Listo!</span>}
+          {...props}
+        />
+      </MemoryRouter>
+    )
+
+    expect(wrapper.find('span').exists()).toBe(true)
+  })
+})
